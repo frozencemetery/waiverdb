da55137bb6639cf66c125889c63548628e77edfb
Matt Jia <mjia@redhat.com>
2017-04-11 13:38:46 +1000

    helper script for tagging releases

---
+++ tag-release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+set -e
+
+version="$1"
+prerelease="$2"
+
+if [ -z "$version" ] ; then
+    echo "Usage: $0 <version> [<prerelease>]" >&2
+    echo "Example: $0 0.1 rc1" >&2
+    exit 1
+fi
+
+if git status --porcelain | grep -q '^.M' ; then
+    echo "Work tree has modifications, stash or add before tagging" >&2
+    exit 1
+fi
+
+sed -i -e "/%global upstream_version /c\%global upstream_version ${version}${prerelease}" waiverdb.spec
+sed -i -e "/^Version:/c\Version:        $version" waiverdb.spec
+if [ -n "$prerelease" ] ; then
+    sed -i -e "/^Release:/c\Release:        0.$prerelease%{?dist}" waiverdb.spec
+else
+    sed -i -e "/^Release:/c\Release:        1%{?dist}" waiverdb.spec
+fi
+sed -i -e "/^version = /c\version = '$version$prerelease'" setup.py
+git add setup.py waiverdb.spec
+git commit -m "Automatic commit of release $version$prerelease"
+git tag -a "waiverdb-$version$prerelease" -m "Tagging release $version$prerelease"
